907f750c6ca1e4763351b05645b6b37f3ec97832
ccox <charles.cox@hbo.com>
2020-02-21 16:34:54 -0800

    new stuff

---
+++ tscribe/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="tscribe",
+    version="1.1.0",
+    author="Robert Williams",
+    author_email="robertedwardwilliams@me.com",
+    description="Produce .docx transcriptions from AWS Transcribe output.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kibaffo33/aws_transcribe_to_docx",
+    packages=setuptools.find_packages(),
+    install_requires=["python-docx", "matplotlib"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+)
